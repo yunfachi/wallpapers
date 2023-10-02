5b1b38f0087514fdd9bbabf56d705e72bb3d1675
yunfachi <yunfachi@gmail.com>
2023-10-02 10:08:01 +0300

    fixed grep

--- random.sh
+++ random.sh
@@ -2,13 +2,13 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-current="${SCRIPT_DIR}/wallpapers/$(swww query | grep -oP 'image: \"\K[^\"]+' | head -1)"
+current="$(swww query | grep -o -m 1 -P '(?<=image: ).*')"
 files=($(find "${SCRIPT_DIR}"/wallpapers/ -type f))
 files=(${files[@]/$current})
 
 if [ ${#files[@]} -ge 1 ]; then
   random="${files[RANDOM % ${#files[@]}]}"
-  swww img ${random}
+  swww img "${random}"
 else
   printf "it was not possible to change the wallpaper because it is either missing or such wallpaper is already installed"
 fi
